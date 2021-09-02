aaebba08f6dc4191a0cfecf4228acf6c27a63a88
wojiazaiyugang <616986514@qq.com>
2021-09-02 14:39:40 +0800

    推理代码

---
+++ inference/__init__.py
@@ -0,0 +1,220 @@
+#
+# Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import argparse
+import os
+
+import cv2
+import numpy as np
+import pycuda.autoinit
+import pycuda.driver as cuda
+import tensorrt as trt
+
+try:
+    # Sometimes python does not understand FileNotFoundError
+    FileNotFoundError
+except NameError:
+    FileNotFoundError = IOError
+
+TRT_LOGGER = trt.Logger()
+EXPLICIT_BATCH = 1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
+
+
+def GiB(val):
+    return val * 1 << 30
+
+
+def add_help(description):
+    parser = argparse.ArgumentParser(description=description, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    args, _ = parser.parse_known_args()
+
+
+def find_sample_data(description="Runs a TensorRT Python sample", subfolder="", find_files=[], err_msg=""):
+    '''
+    Parses sample arguments.
+    Args:
+        description (str): Description of the sample.
+        subfolder (str): The subfolder containing data relevant to this sample
+        find_files (str): A list of filenames to find. Each filename will be replaced with an absolute path.
+    Returns:
+        str: Path of data directory.
+    '''
+
+    # Standard command-line arguments for all samples.
+    kDEFAULT_DATA_ROOT = os.path.join(os.sep, "usr", "src", "tensorrt", "data")
+    parser = argparse.ArgumentParser(description=description, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-d", "--datadir",
+                        help="Location of the TensorRT sample data directory, and any additional data directories.",
+                        action="append",
+                        default=[kDEFAULT_DATA_ROOT])
+    args, _ = parser.parse_known_args()
+
+    def get_data_path(data_dir):
+        # If the subfolder exists, append it to the path, otherwise use the provided path as-is.
+        data_path = os.path.join(data_dir, subfolder)
+        if not os.path.exists(data_path):
+            if data_dir != kDEFAULT_DATA_ROOT:
+                print("WARNING: " + data_path + " does not exist. Trying " + data_dir + " instead.")
+            data_path = data_dir
+        # Make sure data directory exists.
+        if not (os.path.exists(data_path)) and data_dir != kDEFAULT_DATA_ROOT:
+            print("WARNING: {:} does not exist. Please provide the correct data path with the -d option.".format(
+                data_path))
+        return data_path
+
+    data_paths = [get_data_path(data_dir) for data_dir in args.datadir]
+    return data_paths, locate_files(data_paths, find_files, err_msg)
+
+
+def locate_files(data_paths, filenames, err_msg=""):
+    """
+    Locates the specified files in the specified data directories.
+    If a file exists in multiple data directories, the first directory is used.
+    Args:
+        data_paths (List[str]): The data directories.
+        filename (List[str]): The names of the files to find.
+    Returns:
+        List[str]: The absolute paths of the files.
+    Raises:
+        FileNotFoundError if a file could not be located.
+    """
+    found_files = [None] * len(filenames)
+    for data_path in data_paths:
+        # Find all requested files.
+        for index, (found, filename) in enumerate(zip(found_files, filenames)):
+            if not found:
+                file_path = os.path.abspath(os.path.join(data_path, filename))
+                if os.path.exists(file_path):
+                    found_files[index] = file_path
+
+    # Check that all files were found
+    for f, filename in zip(found_files, filenames):
+        if not f or not os.path.exists(f):
+            raise FileNotFoundError(
+                "Could not find {:}. Searched in data paths: {:}\n{:}".format(filename, data_paths, err_msg))
+    return found_files
+
+
+# Simple helper data class that's a little nicer to use than a 2-tuple.
+class HostDeviceMem(object):
+    def __init__(self, host_mem, device_mem):
+        self.host = host_mem
+        self.device = device_mem
+
+    def __str__(self):
+        return "Host:\n" + str(self.host) + "\nDevice:\n" + str(self.device)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+# Allocates all buffers required for an engine, i.e. host/device inputs/outputs.
+def allocate_buffers(engine):
+    inputs = []
+    outputs = []
+    bindings = []
+    stream = cuda.Stream()
+    for binding in engine:
+        size = trt.volume(engine.get_binding_shape(binding)) * engine.max_batch_size
+        dtype = trt.nptype(engine.get_binding_dtype(binding))
+        # Allocate host and device buffers
+        host_mem = cuda.pagelocked_empty(size, dtype)
+        device_mem = cuda.mem_alloc(host_mem.nbytes)
+        # Append the device buffer to device bindings.
+        bindings.append(int(device_mem))
+        # Append to the appropriate list.
+        if engine.binding_is_input(binding):
+            inputs.append(HostDeviceMem(host_mem, device_mem))
+        else:
+            outputs.append(HostDeviceMem(host_mem, device_mem))
+    return inputs, outputs, bindings, stream
+
+
+# This function is generalized for multiple inputs/outputs.
+# inputs and outputs are expected to be lists of HostDeviceMem objects.
+def do_inference(context, bindings, inputs, outputs, stream, batch_size=1):
+    # Transfer input data to the GPU.
+    [cuda.memcpy_htod_async(inp.device, inp.host, stream) for inp in inputs]
+    # Run inference.
+    context.execute_async(batch_size=batch_size, bindings=bindings, stream_handle=stream.handle)
+    # Transfer predictions back from the GPU.
+    [cuda.memcpy_dtoh_async(out.host, out.device, stream) for out in outputs]
+    # Synchronize the stream
+    stream.synchronize()
+    # Return only the host outputs.
+    return [out.host for out in outputs]
+
+
+# This function is generalized for multiple inputs/outputs for full dimension networks.
+# inputs and outputs are expected to be lists of HostDeviceMem objects.
+def do_inference_v2(context, bindings, inputs, outputs, stream):
+    # Transfer input data to the GPU.
+    [cuda.memcpy_htod_async(inp.device, inp.host, stream) for inp in inputs]
+    # Run inference.
+    context.execute_async_v2(bindings=bindings, stream_handle=stream.handle)
+    # Transfer predictions back from the GPU.
+    [cuda.memcpy_dtoh_async(out.host, out.device, stream) for out in outputs]
+    # Synchronize the stream
+    stream.synchronize()
+    # Return only the host outputs.
+    return [out.host for out in outputs]
+
+
+def get_engine(engine_path):
+    # If a serialized engine exists, use it instead of building an engine.
+    print("Reading engine from file {}".format(engine_path))
+    with open(engine_path, "rb") as f, trt.Runtime(TRT_LOGGER) as runtime:
+        return runtime.deserialize_cuda_engine(f.read())
+
+
+def build_engine(onnx_file, trt_file):
+    EXPLICIT_BATCH = 1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
+    batch_size = 1
+    trt_logger = trt.Logger(trt.Logger.WARNING)
+    with trt.Builder(trt_logger) as builder, builder.create_network(EXPLICIT_BATCH) as network, \
+            trt.OnnxParser(network, trt_logger) as parser:
+        builder.max_workspace_size = 1 << 30
+        builder.max_batch_size = batch_size
+        if builder.platform_has_fast_fp16:
+            builder.fp16_mode = True
+        with open(onnx_file, 'rb') as model_file:
+            if not parser.parse(model_file.read()):
+                print('Failed to parse the ONNX file')
+                for err in range(parser.num_errors):
+                    print(parser.get_error(err))
+                return None
+        network.get_input(0).shape = [batch_size, 3, 608, 608]
+        engine = builder.build_cuda_engine(network)
+        if engine is None:
+            print('Failed to build engine')
+            return None
+        print("Completed creating engine")
+        with open(trt_file, 'wb') as engine_file:
+            engine_file.write(engine.serialize())
+        return engine
+
+def load_data(image):
+    image_cv = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image_cv = cv2.resize(image_cv, (224, 224))
+    miu = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    img_np = np.array(image_cv, dtype=float) / 255.
+    r = (img_np[:, :, 0] - miu[0]) / std[0]
+    g = (img_np[:, :, 1] - miu[1]) / std[1]
+    b = (img_np[:, :, 2] - miu[2]) / std[2]
+    img_np_t = np.array([r, g, b])
+    img_np_nchw = np.expand_dims(img_np_t, axis=0)
+    return img_np_nchw.astype(dtype=np.float32).reshape(-1)
---
+++ inference/basketball_detection.py
@@ -0,0 +1,30 @@
+import os
+from datetime import datetime
+
+import cv2
+
+from inference import do_inference
+from inference import get_engine, allocate_buffers, load_data, build_engine
+
+if __name__ == '__main__':
+    onnx_file = r"/home/senseport0/Workspace/HiAlgorithm/mmdetection/tmp.onnx"
+    trt_file = r"/home/senseport0/Workspace/HiAlgorithm/mmdetection/tmp.trt"
+    build_engine(onnx_file, trt_file)
+    # exit()
+
+    # test_dir = r"/home/senseport0/Workspace/HiAlgorithm/mmclassification/data/goal_classification/test"
+    # d = os.path.join(test_dir, "0")
+    # with get_engine(trt_file) as engine, engine.create_execution_context() as context:
+    #     inputs, outputs, bindings, stream = allocate_buffers(engine=engine)
+    #     t, r = 0, 0
+    #     for file in os.listdir(d):
+    #         s = datetime.now()
+    #         if not file.endswith("jpg"):
+    #             continue
+    #
+    #         inputs[0].host = load_data(cv2.imread(os.path.join(d, file)))
+    #         output = do_inference(context=context, bindings=bindings, inputs=inputs, outputs=outputs, stream=stream)
+    #         t += 1
+    #         if output[0][1] < output[0][0]:
+    #             r += 1
+    #         print(f"{r}/{t}: {output[0]} 用时{(datetime.now() - s).total_seconds() * 1000}ms")
