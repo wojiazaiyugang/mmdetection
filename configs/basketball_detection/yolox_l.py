7ea7b5ab0eefbd835f069b9f6e4cc634dabb43db
wojiazaiyugang <616986514@qq.com>
2021-09-10 14:27:48 +0800

    yolox配置

--- configs/basketball_detection/yolox_l.py
+++ configs/basketball_detection/yolox_l.py
@@ -1,4 +1,4 @@
-_base_ = '../ssd/ssd300_coco.py'
+_base_ = '../yolox/yolox_l_8x8_300e_coco.py'
 
 model = dict(
     bbox_head=dict(num_classes=1)
@@ -8,17 +8,6 @@ dataset_type = 'BasketballDetection'
 # data_root = '/mnt/nfs-storage/yujiannan/data/bas_data/train_data/'
 data_root = '/home/senseport0/data/train_data/'
 
-# data = dict(train=dict(type=dataset_type,
-#                        img_prefix=data_root,
-#                        ann_file=data_root + "train_21.3.10_train.json" or "train_21.8.16_train.json"),
-#             val=dict(
-#                 type=dataset_type,
-#                 img_prefix=data_root,
-#                 ann_file=data_root + "train_21.3.10_val.json" or "val_21.8.16.json"),
-#             test=dict(
-#                 type=dataset_type,
-#                 img_prefix=data_root,
-#                 ann_file=data_root + "train_21.3.10_val.json" or "val_21.8.16.json"))
 
 data = dict(
     train=dict(
